439644e11a305a948ccbdf892cb655b8ab313708
nsmotritski <nsmotritski@gmail.com>
2018-04-29 12:18:22 +0300

    Latest update to Exception class

--- CustomException/CarIsDeadException.cs
+++ CustomException/CarIsDeadException.cs
@@ -6,17 +6,16 @@ using System.Threading.Tasks;
 
 namespace CustomException
 {
+    [Serializable]
     public class CarIsDeadException : ApplicationException
     {
-        public DateTime ErrorTimeStamp { get; set; }
-        public string CauseOfError { get; set; }
         public CarIsDeadException() { }
-        //  Передача  сообщения  конструктору  родительского  класса.
-        public CarIsDeadException(string message, string cause, DateTime time)
-        : base(message)
-        {
-            CauseOfError = cause;
-            ErrorTimeStamp = time;
-        }
+        public CarIsDeadException(string message) : base(message) { }
+        public CarIsDeadException(string message,
+        System.Exception inner) : base(message, inner) { }
+        protected CarIsDeadException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        //  Любые  дополнительные  специальные  свойства,  конструкторы  и  члены  данных...
     }
 }
